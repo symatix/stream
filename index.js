00e87cec1ddd5cccbf78b6bf639b8c1592b706bb
Dino Kraljeta <pockshocks@gmail.com>
2018-03-14 11:50:41 +0100

    prebuilt

--- index.js
+++ index.js
@@ -48,7 +48,17 @@ app.post('/api/volume', (req, res) => {
 })
 
 
-
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 app.listen(PORT, () => {
     console.log(`=> server is up on port ${PORT}`);
